584e1e7c67809a96c4b315e2512ffcd71429395d
MichaelSun <zhensherlock@126.com>
2023-03-21 18:17:48 +0800

    feat: add tencent cos function

--- src/utils/tencent.py
+++ src/utils/tencent.py
@@ -3,6 +3,22 @@ from qcloud_cos import CosConfig
 from qcloud_cos import CosS3Client
 
 
+def download_obj(remote_file, local_file):
+    secret_id = os.environ['secret_id']
+    secret_key = os.environ['secret_key']
+    region = os.environ['cos_bucket']
+    bucket = os.environ['cos_region']
+    token = None
+    scheme = 'https'
+
+    config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
+    client = CosS3Client(config)
+    response = client.get_object(
+        Bucket=bucket,
+        Key=remote_file,
+    )
+    response['Body'].get_stream_to_file(local_file)
+
 def upload_cloud():
     secret_id = os.environ['secret_id']
     secret_key = os.environ['secret_key']
