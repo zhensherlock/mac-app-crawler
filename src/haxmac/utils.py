bf5413a63658baa4fca6dbb91b8f5160ffd877ee
MichaelSun <zhensherlock@126.com>
2023-03-24 16:06:23 +0800

    feat: add sqlite function

--- src/haxmac/utils.py
+++ src/haxmac/utils.py
@@ -2,7 +2,7 @@ import requests
 import random
 import re
 from bs4 import BeautifulSoup
-from src.utils.sqlite import SQLiteDB
+# from src.utils.sqlite import SQLiteDB
 
 
 def generate_interval_time():
@@ -57,40 +57,40 @@ def get_data(url):
     }
 
 
-def handle_list(data):
-    for row in data:
-        handle_row_data(row)
+# def handle_list(data):
+#     for row in data:
+#         handle_row_data(row)
+#
 
-
-def handle_row_data(row_data):
-    title = row_data['title']
-    regex = r"(\w+(?:\s+\w+)*)\s+(\d+\.\d+\.\d+)"
-    match = re.match(regex, title)
-    name, version = match.group(1), match.group(2)
-    data = {
-        'title': title,
-        'name': name,
-        'latest_version': version,
-        'description': row_data['description'],
-        'detail_link': row_data['detail_link'],
-        'category_link': row_data['category_link'],
-        'download_link': row_data['download_link']
-    }
-    db = SQLiteDB('haxmac.db')
-    app = db.fetchone('SELECT count(1) FROM mac_app_info WHERE name = ?', name)
-
-    return data
-
-
-def insert_data(data):
-    db = SQLiteDB('haxmac.db')
-    db.execute(
-        'INSERT INTO mac_app_info (name, version, description, download_link, source) VALUES (?, ?, ?, ?, ?)',
-        (
-            data['name'],
-            data['version'],
-            data['description'],
-            data['download_link'],
-            data['source']
-        )
-    )
+# def handle_row_data(row_data):
+#     title = row_data['title']
+#     regex = r"(\w+(?:\s+\w+)*)\s+(\d+\.\d+\.\d+)"
+#     match = re.match(regex, title)
+#     name, version = match.group(1), match.group(2)
+#     data = {
+#         'title': title,
+#         'name': name,
+#         'latest_version': version,
+#         'description': row_data['description'],
+#         'detail_link': row_data['detail_link'],
+#         'category_link': row_data['category_link'],
+#         'download_link': row_data['download_link']
+#     }
+#     db = SQLiteDB('haxmac.db')
+#     app = db.fetchone('SELECT count(1) FROM mac_app_info WHERE name = ?', name)
+#
+#     return data
+#
+#
+# def insert_data(data):
+#     db = SQLiteDB('haxmac.db')
+#     db.execute(
+#         'INSERT INTO mac_app_info (name, version, description, download_link, source) VALUES (?, ?, ?, ?, ?)',
+#         (
+#             data['name'],
+#             data['version'],
+#             data['description'],
+#             data['download_link'],
+#             data['source']
+#         )
+#     )
