e040ff5c4788a0440e1c0636811c5997787387b4
MichaelSun <zhensherlock@126.com>
2023-04-01 23:32:09 +0800

    feat: update global utils

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,1 +1,2 @@
 URL_SEPARATOR = ',||,'
+IMAGE_SEPARATOR = ',||,'
--- src/utils/global_utils.py
+++ src/utils/global_utils.py
@@ -2,9 +2,22 @@ import cloudscraper
 import random
 
 
-def get_page(url):
+def get_page(url, **kwargs):
     scraper = cloudscraper.create_scraper(delay=10, browser={'custom': 'ScraperBot/1.0', })
-    response = scraper.get(url)
+    headers = {
+        'authority': 'xclient.info',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'referer': 'https://xclient.info/',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/111.0.0.0 Safari/537.36'
+    }
+    if kwargs and kwargs['headers'] is not None:
+        headers.update(kwargs['headers'])
+    kwargs.setdefault('headers', headers)
+    response = scraper.get(url, **kwargs)
     if response.status_code == 200:
         return response.text
     return None
